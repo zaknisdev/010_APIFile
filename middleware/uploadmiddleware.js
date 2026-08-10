6e99ba04cd40af6f0d4375a168bc252a83e2e56b
zaknisdev <zakianisf1710@gmail.com>
2026-08-10 14:25:28 +0700

    konfigurasi multer

---
+++ middleware/uploadmiddleware.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) =>  {
+    cb(null, 'uploads/');
+    },
+
+filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  }
+});
+
