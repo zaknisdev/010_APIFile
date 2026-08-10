450c2b197e2ddc902ada53af30cd56f4ff507890
zaknisdev <zakianisf1710@gmail.com>
2026-08-10 15:20:16 +0700

    file config

---
+++ config/config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const development = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+}
+
+module.exports = {development};
